83e1e4d7afa794f40e0d7fbe2bd9df34ea6d1b1c
Djordje Zivanovic <djolezile@gmail.com>
2023-11-27 10:59:07 +0100

    added stark arena daily check

--- main.py
+++ main.py
@@ -103,3 +103,24 @@ def job():
         clear_global()
 
 job()
+
+def check_Arena_Today():
+    URL = 'https://starkarena.co.rs/lat/dogadjaji/'
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36'}
+    page = requests.get(URL, headers=headers)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    head_info = soup.find('div', 'head_info')
+
+    event_date = head_info.find('p', 'datetime').text.strip()[0:2]
+    if(event_date.startswith('0')):
+        event_date = event_date[1]
+
+    event_name = head_info.find('h2').text.strip()
+
+    event_description = head_info.find_all('p')[1].text.strip()
+
+    if(int(event_date) == date.today().day):
+        send_email('ARENA DANAS', '{}\n{}'.format(event_name, event_description))
+
+check_Arena_Today()
